32964f296eb1bf01cf8c4500d16c21435540bedf
jacjozs <36637788+jacjozs@users.noreply.github.com>
2018-07-31 09:51:03 +0200

    Optimalizásciós tesztelöre átírva
    A redukció és a probléma felismerés fejlesztése

--- Optimization/NewtonL/NewtonL.cs
+++ Optimization/NewtonL/NewtonL.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Dynamic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
@@ -10,16 +11,10 @@ using System.Windows;
 
 namespace Optimization
 {
-    /// <summary>
-    /// Az algoritmus csak 2 és 3 dimenziós környezetben volt tesztelve és ott is még csak lineáris emelkedőn!
-    /// Feladatok: (fejlesztés)
-    /// Egyértelműbb megszakitás!
-    /// Kivétel kezelések!
-    /// Olcsóbb logikai menet! (Optimalizálás)
-    /// Leegyszerűsítés!
-    /// </summary>
     public class NewtonL : BaseOptimizationMethod
     {
+        public double HistoryFitness { get; set; }
+        public int Reduct { get; set; }
         protected override void CreateNextGeneration()
         {
             var M1Left = new ArrayList();
@@ -27,31 +22,35 @@ namespace Optimization
             Elements.Sort();
             Elements.RemoveRange(0, Elements.Count - 1);
             var StartPosition = (BaseElement)Elements[0];
-            double LeftUnit, RightUnit;
+            HistoryFitness = ((BaseElement) Elements[0]).Fitness;
             for (int p = 0; p < InitialParameters.Count; p++)
             {
                 M1Left.Clear();
                 M2Right.Clear();
-                foreach (var item in ((BaseElement)StartPosition).Position)
+                foreach (var item in StartPosition.Position)
                 {
                     M1Left.Add(item);
                     M2Right.Add(item);
                 }
-                LeftUnit = ((double)LowerParamBounds[p] - (double)StartPosition[p]) / 50;
-                RightUnit = ((double)UpperParamBounds[p] - (double)StartPosition[p]) / 50;
 
-                M1Left[p] = (double)M1Left[p] + LeftUnit;
-                M2Right[p] = (double)M2Right[p] + RightUnit;
+                M1Left[p] = (double)M1Left[p] + ((double)LowerParamBounds[p] - (double)StartPosition[p]) / Reduct;
+                M2Right[p] = (double)M2Right[p] + ((double)UpperParamBounds[p] - (double)StartPosition[p]) / Reduct;
 
-                BaseElement Leftt = (BaseElement)GetNewElement(FitnessFunction, M1Left);
-                BaseElement Rightt = (BaseElement)GetNewElement(FitnessFunction, M2Right);
-                if (Leftt.Fitness > Rightt.Fitness)
-                    StartPosition = Rightt;
-                if (Leftt.Fitness < Rightt.Fitness)
-                    StartPosition = Leftt;
+                BaseElement M1 = (BaseElement)GetNewElement(FitnessFunction, M1Left);
+                BaseElement M2 = (BaseElement)GetNewElement(FitnessFunction, M2Right);
+                if (M1.Fitness > M2.Fitness)
+                    StartPosition = M2;
+                if (M1.Fitness < M2.Fitness)
+                    StartPosition = M1;
                 if (Integer[p])
                     StartPosition[p] = Math.Round((double)StartPosition[p]);
                 Elements[0] = StartPosition;
+                if (HistoryFitness < ((BaseElement) Elements[0]).Fitness)
+                    Reduct++;
+                else if (HistoryFitness == ((BaseElement) Elements[0]).Fitness)
+                    Reduct += 2;
+                else
+                    HistoryFitness = StartPosition.Fitness;
             }
         }
     }
