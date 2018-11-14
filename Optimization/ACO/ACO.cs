e14ec7303860a8b55f2585c41bb29c21148a1f1e
jacjozs <36637788+jacjozs@users.noreply.github.com>
2018-11-14 14:29:07 +0100

    Új hangya alap

---
+++ Optimization/ACO/ACO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Optimization
+{
+    public class ACO : BaseOptimizationMethod
+    {
+        protected override void CreateNextGeneration()
+        {
+
+        }
+    }
+}
